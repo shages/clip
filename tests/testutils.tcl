f1482a670a2e6f7d7bbbaad55624125e7e07f442
Erik Ronshagen <eronshagen@gmail.com>
2016-09-18 23:15:40 -0700

    Added initial unit tests

---
+++ tests/testutils.tcl
@@ -0,0 +1,65 @@
+
+proc _init {} {
+  namespace eval ___test {
+    variable total_tests
+    variable total_errors
+    variable msg
+    variable err
+  }
+}
+
+proc _summarize {} {
+  puts "######################"
+  if {$___test::total_errors} {
+    puts "## ERRORS"
+  } else {
+    puts "## PASS"
+  }
+
+  set pass_rate [expr {1 - $___test::total_errors/$___test::total_tests}]
+  puts "######################"
+  puts "Summary:"
+  puts " Total tests: $___test::total_tests"
+  puts " Failed tests: $___test::total_errors"
+  puts " Pass rate: [format {%.1f%%} $pass_rate]"
+}
+
+proc _suite {name tests} {
+  puts "######################"
+  puts "## Test: $name"
+  set count 0
+  set errcount 0
+  foreach test $tests {
+    puts "Running subtest $count..."
+    if {[catch {set r [eval $test]} ___test::msg ___test::err]} {
+      puts "ERROR: Exception during test: [dict get $___test::err -errorinfo]"
+      incr errcount
+    }
+    #elseif {!$r}
+    #  puts "ERROR: Incorrect result for test: $test"
+    #  incr errcount
+    incr count
+  }
+
+  # Record summary
+  incr ___test::total_tests $count
+  incr ___test::total_errors $errcount
+
+  puts "Summary:"
+  puts " Total tests: $count"
+  puts " Failed tests: $errcount"
+  puts " Pass rate: [format {%.1f%%} [expr {100*(1-$errcount/$count)}]]"
+  puts ""
+  return $errcount
+}
+
+proc _assert_eq {a b} {
+  if {$a == $b} {
+    return 0
+  } else {
+    puts "ERROR: Not equal:"
+    puts " a: $a"
+    puts " b: $b"
+    error AssertionError
+  }
+}
---
+++ tests/units.tcl
@@ -0,0 +1,66 @@
+#!/usr/bin/env tclsh
+
+lappend auto_path [file normalize [file join [pwd] ..]]
+package require ghclip
+
+source ./testutils.tcl
+
+_init
+
+# lshift
+_suite "lshift" {
+  {
+    set var {}
+    catch {[lshift var]} msg err
+    _assert_eq [dict get $err -errorstack INNER returnStk] Empty
+  }
+  {
+    set var {0}
+    #expr {[lshift var] == 0 && $var == {}}
+    _assert_eq [lshift var] 0
+    _assert_eq $var {}
+  }
+  {
+    set var {0 1 2}
+    _assert_eq [lshift var] 0
+    _assert_eq $var {1 2}
+  }
+}
+
+# Example assertion error
+#_suite "lshift2" {
+#  {
+#    set var {0 0}
+#    lshift var
+#    _assert_eq $var {0 0}
+#  }
+#}
+
+# elaborate_expression
+_suite "elaborate_expression" {
+  {
+    _assert_eq [elaborate_expression {A AND B}] {A AND B}
+  }
+  {
+    _assert_eq [elaborate_expression {A XOR B}] {A XOR B}
+  }
+  {
+    _assert_eq [elaborate_expression {A OR B}] {A OR B}
+  }
+  {
+    _assert_eq [elaborate_expression {A OR B AND C XOR A}] {A OR B AND C XOR A}
+  }
+  {
+    _assert_eq [elaborate_expression {A ANDNOT B}] {A XOR B AND A}
+  }
+  {
+    # Wrong expectation
+    # Should be:
+    # (A OR B) XOR C AND (A OR B)
+    _assert_eq [elaborate_expression {A OR B ANDNOT C}] {A OR B XOR C AND B}
+  }
+}
+#_suite [elaborate {A ANDNOT B}] == {A XOR B AND A}
+#_suite [elaborate {A XOR B ANDNOT C}] == {A XOR B XOR C AND {A XOR B}}
+
+_summarize
